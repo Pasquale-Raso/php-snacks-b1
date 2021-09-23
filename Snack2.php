e0907bcf0751f49e6ccf7c42e67a9e76ea9abf8d
Pasquale-Raso <padqualeraso@gmail.com>
2021-09-23 14:45:17 +0200

    base

---
+++ Snack2.php
@@ -0,0 +1,4 @@
+<!-- SNACK 2
+Creare un array contenente qualche alunno di un’ipotetica classe.
+Ogni alunno avrà Nome, Cognome e un array contenente i suoi voti scolastici (senza, materia, solo numeri). Creare una funzione che calcoli la media voto di un alunno.
+Stampare Nome, Cognome e la media dei voti di ogni alunno. -->
